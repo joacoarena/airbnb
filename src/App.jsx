c821ebcaf15654312367e30d22689650a45ea4ac
joacoarena <joaquin.t.arena@gmail.com>
2023-09-02 18:02:36 -0300

    stack

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Grid from './components/Grid'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <Grid/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    
+    <nav className='nav'>
+        <img src="../images/airbnb-logo.png" className='logo' alt="f" />
+        
+    </nav>
+    )
+}
+
+export default Navbar
